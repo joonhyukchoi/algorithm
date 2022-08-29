ff9f9e471384551daad4f2750eb28dada43aa0ca
joonhyukchoi <cjh4787@gmail.com>
2022-08-29 13:14:27 +0900

    feat: 구현문제 배열 초기화 삽질, 변경 방향 삽질

--- programmers/행렬테두리회전하기.py
+++ programmers/행렬테두리회전하기.py
@@ -1,39 +1,25 @@
-# def solution(rows, columns, queries):
-#     answer = []
-#     array = [[j + rows * i for j in range(1, columns + 1)] for i in range(rows)]
-    
-#     for query in queries:
-#         x1, y1, x2, y2 = query
-#         dx = x2 - x1
-#         dy = y2 - y1
-#         axisx = [x1, x2, x2, y2]
-#         axisy = [y1, y1, y2, y2]
-#         my = [1, 0, -1, 0]
-#         mx = [0, -1, 0, 1]
-#         distances = [dy, dx, dy, dx]
-#         x, y = x1, y1
-#         for i in range(4):
-#             first = array[axisx[i]][axisy[i]]
-#             print(first)
-#             for dis in range(distances[i]):
-#                 print(x, y, mx[i], my[i], dis)
-#                 if dis == 0:
-#                     temp = array[x + mx[i]][y + my[i]]
-#                     array[x + mx[i]][y + my[i]] = first
-#                     first = temp
-#                 else:
-#                     x = x + mx[i] 
-#                     y = y + my[i]
-#                     temp = array[x + mx[i]][y + my[i]]
-#                     array[x + mx[i]][y + my[i]] = first
-#                     first = temp
-            
-#     return answer
-
-# rows = 6
-# columns = 6
-# queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
-# print(solution(rows, columns, queries))
-
-str1 = 'cba'
-print(''.join(sorted(str1)))
+def solution(rows, columns, queries):
+    answer = []
+    array = [[j + columns * i for j in range(1, columns + 1)] for i in range(rows)]
+    for query in queries:
+        x1, y1, x2, y2 = query
+        dx = x2 - x1
+        dy = y2 - y1
+        axisx = [x2, x2, x1, x1]
+        axisy = [y1, y2, y2, y1]
+        mx = [0, -1, 0, 1]
+        my = [1, 0, -1, 0]
+        distances = [dy, dx, dy, dx]
+        tempStart = array[axisx[0] - 1][axisy[0] - 1]
+        tempMin = tempStart
+        for i in range(4):
+            startx, starty = axisx[i] - 1, axisy[i] - 1
+            for dis in range(distances[i]):
+                x = startx + dis * mx[i] 
+                y = starty + dis * my[i]
+                array[x][y] = array[x + mx[i]][y + my[i]]
+                tempMin = min(tempMin, array[x][y])
+                # print(x, y, mx[i], my[i], array)
+        array[axisx[0] - 2][axisy[0] - 1] = tempStart
+        answer.append(tempMin)
+    return answer
