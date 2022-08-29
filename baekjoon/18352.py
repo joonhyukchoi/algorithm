e9b2392e99b5f095eaa4039dbacdf99db3775cb7
joonhyukchoi <cjh4787@gmail.com>
2022-08-29 23:59:36 +0900

    feat: bfs 특정거리의 도시찾기

---
+++ baekjoon/18352.py
@@ -0,0 +1,30 @@
+import sys
+from collections import defaultdict
+from collections import deque
+
+inp = sys.stdin.readline
+
+N, M, K, X = map(int, inp().split())
+graph = [[] for _ in range(n + 1)]
+for i in range(M):
+    a, b = map(int, inp().split())
+    graph[a].append(b)
+
+dist = [-1] * (N + 1)
+dist[X] = 0
+
+q = deque([X])
+while q:
+    now = q.popleft()
+    for next_node in graph[now]:
+        if dist[next_node] == -1:
+            dist[next_node] = dist[now] + 1
+            q.append(next_node)
+
+check = False
+for i in range(1, N + 1):
+    if dist[i] == K:
+        print(i)
+        check = True
+if check == False:
+    print(-1)
