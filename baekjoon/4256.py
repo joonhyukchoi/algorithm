e99dc75a128c0b00767ecb238ab70b293016768f
joonhyukchoi <cjh4787@gmail.com>
2022-08-31 15:55:36 +0900

    feat: 복습하기 재귀 트리 array[:] index 할때 index 초과해도 마지막 인덱스값까지만 알아서 나옴

---
+++ baekjoon/4256.py
@@ -0,0 +1,22 @@
+import sys
+inp = sys.stdin.readline
+n = int(inp())
+
+def postorder(preorder, inorder):
+    if len(preorder) == 0:
+        return
+    if len(inorder) == 0:
+        return
+    idx = inorder.index(preorder[0])
+    postorder(preorder[1:idx + 1], inorder[0:idx])
+    postorder(preorder[idx + 1:], inorder[idx + 1:])
+    print(preorder[0], end = ' ')
+
+for _ in range(n):
+    prlist = list(map(int, inp().split()))
+    inlist = list(map(int, inp().split()))
+    postorder(prlist, inlist)
+    print()
+
+# nums = [1, 2, 3, 4, 5]
+# print(nums[1:60])
