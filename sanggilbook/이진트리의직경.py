5ae4a896f599edd05b19b794284ce4255c7932dd
joonhyukchoi <cjh4787@gmail.com>
2022-09-21 22:11:18 +0900

    feat: 복습필요 이진트리

---
+++ sanggilbook/이진트리의직경.py
@@ -0,0 +1,15 @@
+class Solution:
+    longest: int = 0
+
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        def dfs(node: TreeNode) -> int:
+            if not node:
+                return -1
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            self.longest = max(self.longest, left + right + 2)
+            return max(left, right) + 1
+        
+        dfs(root)
+        return longest
