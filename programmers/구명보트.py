a0dbcaeb4e508db3e9c2deae288e6574ed55c1c6
joonhyukchoi <cjh4787@gmail.com>
2022-09-25 21:47:51 +0900

    feat: linear queue

--- programmers/구명보트.py
+++ programmers/구명보트.py
@@ -1,13 +1,31 @@
-from collections import deque
-def solution(people, limit):
-    q = deque()
-    people.sort()
-    for el in people:
-        q.append(el)
-    answer = 0
-    while q:
-        big = q.pop()
-        if q and big + q[0] <= limit:
-            q.popleft()
-        answer += 1
-    return answer
+# from collections import deque
+# def solution(people, limit):
+#     q = deque()
+#     people.sort()
+#     for el in people:
+#         q.append(el)
+#     answer = 0
+#     while q:
+#         big = q.pop()
+#         if q and big + q[0] <= limit:
+#             q.popleft()
+#         answer += 1
+#     return answer
+S = 0
+S |= 1 << 10
+S |= 1 << 4
+# S &= ~(1 << 4)
+print(S & 1 << 4)
+test = [1, 2, 3]
+s1 = set(test)
+s2 = set()
+s1.add(1)
+s1.add(4)
+s2.add(3)
+print(s1)
+
+import collections
+import itertools
+
+outpuet = collections.Counter([1,2,3,3,4])
+print(outpuet.most_common(1))
