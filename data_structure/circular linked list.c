bc91213c807a85b92b034b451f2e576daf800a57
joonhyukchoi <cjh4787@gmail.com>
2022-09-28 19:46:36 +0900

    feat: 원형 연결리스트 구현

---
+++ data_structure/circular linked list.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct ListNode { 
+    element data;
+    struct ListNode *link;
+} ListNode;
+
+ListNode* insert_first(ListNode* head, element data)
+{
+    ListNode *node = (ListNode *)malloc(sizeof(ListNode));
+    node->data = data;
+    if (head == NULL) {
+        head = node;
+        node->link = head;
+    }
+    else {
+        node->link = head->link;
+        head->link = node;
+    }
+    return head;
+}
+
+// 노드 pre 뒤에 새로운 노드 삽입, pre는 따로 search 해줘야 함
+ListNode* insert_last(ListNode *head, element value)
+{
+    ListNode *node = (ListNode *)malloc(sizeof(ListNode));
+    node->data = data;
+    if (head == NULL) {
+        head = node;
+        node->link = head;
+    } else {
+        node->link = head->link;
+        head->link = node;
+        head = node;
+    }
+    return head;
+}
+
+void print_list(ListNode *head)
+{
+    ListNode* p;
+
+    if (head == NULL) return;
+    p = head->link;
+    do {
+        printf("%d->", p->data);
+        p = p->link;
+    } while (p != head->link);
+}
+
