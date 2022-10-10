6a73f1f126cf953403c2165b619e7f373c9fa128
joonhyukchoi <cjh4787@gmail.com>
2022-10-10 20:13:41 +0900

    feat: 리스트 이용한 스택 구현 추가

--- data_structure/stack.c
+++ data_structure/stack.c
@@ -1,3 +1,5 @@
+// 배열을 이용한 구현
+
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_STACK_SIZE 100
@@ -45,4 +47,72 @@ element pop(StackType *s)
         exit(1);
     }
     else return s->data[(s->top)--];
+}
+
+// 리스트를 이용한 구현
+
+typedef int element;
+typedef struct StackNode {
+    element data;
+    struct StackNode* link;
+} StackNode;
+
+typedef struct {
+    StackNode* top;
+} LinkedStackType;
+
+// 초기화 함수
+void init(LinkedStackType* s)
+{
+    s->top = NULL;
+}
+
+// 공백 상태 검출 함수
+int is_empty(LinkedStackType* s)
+{
+    return (s->top == NULL);
+}
+
+// 삽입 함수
+void push(LinkedStackType* s, element item)
+{
+    StackNode* temp = (StackNode*)malloc(sizeof(StackNode));
+    temp->data = item;
+    temp->link = s->top;
+    s->top = temp;
+}
+
+// 출력
+void print_stack(LinkedStackType* s)
+{
+    for (StackNode* p = s->top; p != NULL; p = p->link)
+        printf("%d->", p->data);
+    printf("NULL \n");
+}
+
+// 삭제 함수
+element pop(LinkedStackType* s)
+{
+    if (is_empty(s)) {
+        fprintf(stderr, "스택이 비어있음\n");
+        exit(-1);
+    } else {
+        StackNode* temp = s->top;
+        int data = temp->data;
+        s->top = s->top->link;
+        free(temp);
+        return data;
+    }
+}
+
+// 피크 함수
+element peek(LinkedStackType* s)
+{
+    if (is_empty(s)) {
+        fprintf(stderr, "스택이 비어있음\n");
+        exit(1);
+    }
+    else {
+        return s->top->data;
+    }
 }
