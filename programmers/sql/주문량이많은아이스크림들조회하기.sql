a3107ad6c78b252caf032b9be9869127f3d83f4e
joonhyukchoi <cjh4787@gmail.com>
2023-01-04 23:58:41 +0900

    Create 주문량이많은아이스크림들조회하기.sql

---
+++ programmers/sql/주문량이많은아이스크림들조회하기.sql
@@ -0,0 +1,8 @@
+select t1.flavor from (
+select flavor, sum(total_order) as s from july group by flavor)
+as t1
+left outer join
+first_half as t2
+on t1.flavor = t2.flavor
+order by (t1.s + t2.flavor) desc
+limit 3
